601b8ddb5a1bb83710bdc5eb58094d50d66b5a9c
Bjørnar <598062@stud.hvl.no>
2024-01-25 14:18:40 +0100

    noen flere tester

--- DAT109-Oblig1/src/test/java/no/hvl/dat109/oblig1/BrettTest.java
+++ DAT109-Oblig1/src/test/java/no/hvl/dat109/oblig1/BrettTest.java
@@ -1,23 +1,35 @@
 package no.hvl.dat109.oblig1;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BrettTest {
 
+    static Brett brett;
+
+    @BeforeAll
+    static void setupBrett() {
+        brett = new Brett();
+    }
+
     @Test
     void getStartposisjon() {
-       Brett brett = new Brett();
-
         assertEquals(0, brett.getStartposisjon().getRutePosisjon());
     }
 
     @Test
     void finnRute() {
-        Brett brett = new Brett();
         Rute rute = brett.finnRute(5);
         assertNotNull(rute);
         assertEquals(5, rute.getRutePosisjon());
+    }
+
+    @Test
+    void testHarStige() {
+        Rute stigeSlutt = brett.harStige(new Rute(43));
+        assertNotNull(stigeSlutt);
+        assertEquals(new Rute(62), stigeSlutt);
     }
 }
--- DAT109-Oblig1/src/test/java/no/hvl/dat109/oblig1/SpillerTest.java
+++ DAT109-Oblig1/src/test/java/no/hvl/dat109/oblig1/SpillerTest.java
@@ -1,19 +1,20 @@
 package no.hvl.dat109.oblig1;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpillerTest {
 
-    @Test
-    void spillTrekk() {
-
+    static Brett brett;
+    @BeforeAll
+    static void setupBrett(){
+        brett = new Brett();
     }
 
     @Test
     void getNavn() {
-        Brett brett = new Brett();
         Spiller spiller = new Spiller("Player1", "red", brett);
 
         assertEquals("Player1", spiller.getNavn());
@@ -21,10 +22,61 @@ class SpillerTest {
 
     @Test
     void getBrikke() {
-        Brett brett = new Brett();
         Spiller spiller = new Spiller("Player1", "red", brett);
 
         assertNotNull(spiller.getBrikke());
         assertEquals(brett.getStartposisjon(), spiller.getBrikke().getPosisjon());
+    }
+
+    @Test
+    void testFlyttBrikkeTilRiktigPos() {
+        int verdi = 2;
+        Spiller spiller = new Spiller("noe", "noe", brett);
+        int posisjon = spiller.getBrikke()
+                              .hentRutePosisjon();
+        spiller.spillTrekk(new Terning() {
+            @Override
+            public int trill() {
+                return verdi;
+            }
+        });
+
+        assertEquals(posisjon + verdi,
+                     spiller.getBrikke()
+                            .hentRutePosisjon());
+    }
+
+    @Test
+    void testStiger() {
+
+        // stige fra 43 -> 62
+        int verdi = 43;
+        Spiller spiller = new Spiller("noe", "noe", brett);
+        spiller.spillTrekk(new Terning() {
+            @Override
+            public int trill() {
+                return verdi;
+            }
+        });
+
+        assertEquals(
+                62,
+                spiller.getBrikke()
+                       .hentRutePosisjon());
+
+        int posisjon = spiller.getBrikke()
+                              .hentRutePosisjon();
+
+        spiller.spillTrekk(new Terning() {
+            @Override
+            public int trill() {
+                return 74 - posisjon;
+            }
+        });
+
+        assertEquals(
+                12,
+                spiller.getBrikke()
+                       .hentRutePosisjon());
     }
 }
